38e2540fc27bdf7a3b5bf0e9527e24dd5cd5dce6
Wallace <wbalaniu@my.centennialcollege.ca>
2019-06-25 10:07:04 -0400

    Added TypeScript

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,8 +1,10 @@
-(function () {
+(function() {
+
     function Init() {
         console.log("Initialization Start");
-        var x = 10;
+
+        let x: number = 10;
     }
+
     window.onload = Init;
 })();
-//# sourceMappingURL=game.js.map
